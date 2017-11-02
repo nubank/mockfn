78d3a81154c8bc5dd91622a616a498cb253b8ce3
Pedro Matiello <pedro@pmatiello.me>
2017-11-02 17:10:42 -0200

    Write project description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject mockfn "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A library for mocking Clojure functions."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
