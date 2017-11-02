cba82684bbfb60ec39e649e53941c84f23965498
Pedro Matiello <pedro@pmatiello.me>
2017-11-02 17:04:43 -0200

    Create project.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mockfn
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/mockfn/core_test.clj
@@ -0,0 +1,7 @@
+(ns mockfn.core-test
+  (:require [clojure.test :refer :all]
+            [mockfn.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
