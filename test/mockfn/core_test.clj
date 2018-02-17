ece733f513af477eb2b1fafcf6acd33afabe8abc
Pedro Matiello <pedro@pmatiello.me>
2018-02-17 15:34:58 -0200

    Remove unnecessary tests.

--- test/mockfn/core_test.clj
+++ test/mockfn/core_test.clj
@@ -1,15 +1,2 @@
 (ns mockfn.core-test
-  (:require [clojure.test :refer :all]
-            [mockfn.core :refer :all]))
-
-(def one-fn)
-
-(deftest providing-test
-  (providing
-    [(one-fn) :mocked]
-    (is (= :mocked (one-fn)))))
-
-(deftest verifying-test
-  (verifying
-    [(one-fn) :mocked (at-least 1)]
-    (is (= :mocked (one-fn)))))
+  (:require [clojure.test :refer :all]))
