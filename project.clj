da83da4cd5a74c5fddd712e2fcc096049404c1b8
Pedro Matiello <pedro@pmatiello.me>
2018-03-18 12:10:00 -0300

    Set project url in project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mockfn "0.1.0-SNAPSHOT"
   :description "A library for mocking Clojure functions."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/pmatiello/mockfn"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]])
