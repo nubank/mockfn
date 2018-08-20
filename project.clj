10867b7cf98828fab0fa61f03d17618c93212781
Pedro Matiello <pedro@pmatiello.me>
2018-08-20 19:31:58 -0300

    Release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mockfn "0.1.0"
+(defproject mockfn "0.2.0"
   :description "A library for mocking Clojure functions."
   :url "https://github.com/pmatiello/mockfn"
   :license {:name "Eclipse Public License"
